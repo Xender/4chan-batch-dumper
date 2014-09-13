f64670bd673d1af256ee2e17e845da594e429130
Aleksander Nitecki <ixendr@itogi.re>
2014-09-14 00:07:31 +0200

    Use thread names ("semantic URL" parts) in thread info filename.

--- 4chan.py
+++ 4chan.py
@@ -25,7 +25,8 @@ def scrape(thread_url):
 
 	data = r.json()
 
-	with open('thread.{board}.{thread_id}.json'.format(**locals()), 'w') as thread_f:
+	semantic_url = data['posts'][0]['semantic_url']
+	with open('thread.{board}.{thread_id}.{semantic_url}.json'.format(**locals()), 'w') as thread_f:
 		json.dump(data, thread_f,
 			sort_keys=True,
 			ensure_ascii=False,
